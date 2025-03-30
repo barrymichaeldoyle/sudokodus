eb6a237702db1b3ac61a4917cb6920e93abdba62
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2025-03-30 15:21:44 +0200

    setup basic navigation entry point to ap

---
+++ app/_layout.tsx
@@ -0,0 +1,32 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="about"
+        options={{
+          title: 'About',
+          headerShown: true,
+        }}
+      />
+    </Stack>
+  );
+}
